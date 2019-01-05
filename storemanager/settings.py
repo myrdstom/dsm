e425ac4a8481cf447c2d327760a315d35ebec874
myrdstom <nserekopaul@gmail.com>
2019-01-05 14:38:09 +0300

    Improving settings

--- storemanager/settings.py
+++ storemanager/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'storemanager.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 3,
 
 }
 
